710004ebfc60a5bfedaa66bce96531296d7b54dc
Athul krishna K S <athulkrishna736@gmail.com>
2025-02-28 23:23:20 +0530

    trie auto complete and delete workouted

--- trie.js
+++ trie.js
@@ -119,6 +119,73 @@ class Trie1 {
     }
 
     autocomplete(prefix) {
+        let node = this.root;
+
+        for (let char of prefix) {
+            if (!node.children[char]) return [];
+            node = node.children[char];
+        }
+
+        let result = [];
+
+        function dfs(currNode, currPrefix) {
+            if (currNode.isEnd) result.push(currPrefix);
+
+            for (let child in currNode.children) {
+                dfs(currNode.children[child], currPrefix + child);
+            }
+        };
+
+        dfs(node, prefix);
+        return result;
+    }
+
+}
+
+const trie1 = new Trie1()
+
+// trie1.insert('ban')
+// trie1.insert('bat')
+// trie1.insert('apply')
+// console.dir(trie1.root, { depth: null })
+// console.log(trie1.search('ban'))
+// console.log(trie1.search('banned'))
+// console.log(trie1.autocomplete('b'))
+
+
+class TrieNode2 {
+    constructor() {
+        this.children = {}
+        this.isEnd = false
+    }
+}
+
+class Trie2 {
+    constructor() {
+        this.root = new TrieNode2()
+    }
+
+    insert(word) {
+        let node = this.root
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode2()
+            }
+            node = node.children[char]
+        }
+        node.isEnd = true
+    }
+
+    search(word) {
+        let node = this.root
+        for (let char of word) {
+            if (!node.children[char]) return false
+            node = node.children[char]
+        }
+        return node.isEnd
+    }
+
+    autocomplete(prefix) {
         let node = this.root
 
         for (let char of prefix) {
@@ -127,21 +194,28 @@ class Trie1 {
         }
 
         let result = []
-        const dfs = (currNode, currPrefix) => {
-            if (currNode.isEnd) result.push(currPrefix)
 
-            for (let child in currNode.children) {
-                dfs(currNode.children[child], currPrefix + child)
+        function dfs(node, prefix) {
+            if (node.isEnd) {
+                result.push(prefix)
+            }
+
+            for (let child in node.children) {
+                dfs(node.children[child], prefix + child)
             }
         }
+        dfs(node, prefix)
+        return result
     }
 }
 
-const trie1 = new Trie1()
+const trie2 = new Trie2()
 
-trie1.insert('ban')
-trie1.insert('bat')
-trie1.insert('apply')
-console.dir(trie1.root, { depth: null })
-console.log(trie1.search('ban'))
-console.log(trie1.search('banned'))
+trie2.insert('Apple')
+trie2.insert('Apply')
+trie2.insert('batman')
+trie2.insert('batsman')
+trie2.insert('Ape')
+
+console.log(trie2.search('Apple'))
+console.log(trie2.autocomplete('Ap'))
