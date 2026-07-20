512c2f1bbd4bb44a30bb00178c307d360b440fdf
AthulKrishna-736 <athulkrishna736@gmail.com>
2026-07-20 23:57:35 +0530

    learning array operation using go

--- DSA_Golang/array/array.go
+++ DSA_Golang/array/array.go
@@ -3,8 +3,13 @@ package array
 import "fmt"
 
 func LearnArrays() {
-	basicArrays()
-	totalSum()
+	// basicArrays()
+	// totalSum()
+	// findMax()
+	// findMin()
+	// countEvenOdd()
+	// findElements()
+	insetElemnentAtPos()
 }
 
 func basicArrays() {
@@ -46,4 +51,96 @@ func totalSum() {
 	}
 
 	fmt.Printf("Total sum with range for loop: %d\n", sum)
+}
+
+func findMax() {
+	arr := []int{-3, -50, -4, 0}
+
+	if len(arr) == 0 {
+		fmt.Printf("Zero length array\n")
+		return
+	}
+
+	max := arr[0]
+
+	for _, val := range arr {
+		if val > max {
+			max = val
+		}
+	}
+
+	fmt.Printf("Max Element in array: %d, arr:= %v", max, arr)
+}
+
+func findMin() {
+	arr := []int{-3, 1, 0, 40, 2, -1}
+
+	if len(arr) == 0 {
+		fmt.Printf("Empty array\n")
+		return
+	}
+
+	min := arr[0]
+
+	for _, val := range arr {
+		if val < min {
+			min = val
+		}
+	}
+
+	fmt.Printf("Minimum Value: %d, arr=%v", min, arr)
+}
+
+func countEvenOdd() {
+	arr := []int{1, 2, 3, 4, 5, 6}
+
+	oddCount := 0
+	evenCount := 0
+
+	for _, val := range arr {
+		if val%2 == 0 {
+			evenCount++
+		} else {
+			oddCount++
+		}
+	}
+
+	fmt.Printf("Odd Count: %d\nEven Count: %d\n", oddCount, evenCount)
+}
+
+func findElements() {
+	arr := []int{10, 20, 30, 40, 50}
+	target := 30
+
+	for i, val := range arr {
+		if val == target {
+			fmt.Printf("Element found: %d", i)
+			return
+		}
+	}
+
+	fmt.Printf("Element not found")
+}
+
+func insetElemnentAtPos() {
+	arr := []int{1, 2, 3, 4, 5, 6}
+	pos := 3
+	val := 10
+
+	if pos >= len(arr) {
+		arr = append(arr, val)
+	} else {
+		arr = append(arr, 0)
+	}
+
+	for i := len(arr) - 1; i >= 0; i-- {
+		if pos == i {
+			arr[i+1] = arr[i]
+			arr[i] = val
+		} else if pos <= i {
+			arr[i+1] = arr[i]
+		}
+	}
+
+	fmt.Printf("After inserting Element: %v", arr)
 }
